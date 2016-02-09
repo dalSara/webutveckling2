6867faea093f2e338683845cf7d139fb980ba9b8
sillyTroll <saradalfalk@gmail.com>
2016-02-09 21:11:12 +0100

    bara en liten børjan på js/jquery

---
+++ Javascript/ModulePattern-utkast.js
@@ -0,0 +1,30 @@
+//Module Pattern (Bare en start.. jag måste børja nånstans..=)
+
+var IMAGEMODUL =(function(){}{
+
+    var imagelistJSON = {imagelist:[
+        {title: "1.jpg"},
+        {title: "2.jpg"},
+        {title: "3.jpg"},
+        {title: "4.jpg"},
+        {title: "5.jpg"}
+
+    ]};
+
+    var getImage = function(){
+        return imagelistJSON.imagelist[index];
+
+    }
+
+    return{
+        getImage: getImage
+
+    };
+
+
+}());
+
+//en annen app
+
+IMAGEMODUL.getImage(0);
+
