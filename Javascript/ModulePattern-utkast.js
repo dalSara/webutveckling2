6e30aa3fb739f3ae611f46a701434dc158d75c3d
nuranes <uranes743@hotmail.com>
2016-02-16 11:58:55 +0100

    la til metoden for scrollTxt, ikkje fungerande enda

--- Javascript/ModulePattern-utkast.js
+++ Javascript/ModulePattern-utkast.js
@@ -1,27 +1,38 @@
 //Module Pattern (Bare en start.. jag måste børja nånstans..=) endre gjerne.. er ikke sikker på vad jeg gør..)
 
-var IMAGEMODUL =(function(){}{
+var IMAGEMODUL =(function(){
 
     var imagelistJSON = {imagelist:[
-        {title: "1.jpg"},
-        {title: "2.jpg"},
-        {title: "3.jpg"},
-        {title: "4.jpg"},
-        {title: "5.jpg"}
+        {"title": "1.jpg"},
+        {"title": "2.jpg"},
+        {"title": "3.jpg"},
+        {"title": "4.jpg"},
+        {"title": "5.jpg"}
 
     ]};
 
     var getImage = function(){
-        return imagelistJSON.imagelist[index];
-
-    }
-
-    return{
-        getImage: getImage
-
+        return imagelistJSON.imagelist[index].title;
     };
 
+    var scrollTxt = function(){
+        var distancePerSec = 50;
 
+        $(".txtSec").mouseover(function () {
+            var h = $(".txtSec").height();
+            var targetScrollTop = h + $(window).height();
+            var distanceToTravel = targetScrollTop - $(window).scrollTop();
+            var animationDuration = (distanceToTravel / distancePerSec) * 1000;
+
+            $("html, body").animate({
+                scrollTop: targetScrollTop
+            }, animationDuration, 'linear');
+        }
+                              )};
+    return{
+        getImage: getImage,
+        scrollTxt: scrollTxt
+    };
 }());
 
 //en annen app
